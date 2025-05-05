952ebf301961a132b35453bc3ee5822de3a08aca
fgallardo@favric.cl <sadic@MacBook-Pro-de-Felipe.local>
2025-05-05 18:46:17 -0400

    new branches

--- src/app/domains/check-list/check-list.routes.ts
+++ src/app/domains/check-list/check-list.routes.ts
@@ -1,13 +1,23 @@
 import { Routes } from '@angular/router';
 import { CheckListLayoutComponent } from './layout/check-list-layout/check-list-layout.component';
 import { CheckListDashboardComponent } from './pages/check-list-dashboard/check-list-dashboard.component';
+import { CheckListAccessComponent } from './pages/check-list-access/check-list-access.component';
+import { ChangePasswordsComponent } from './pages/change-passwords/change-passwords.component';
+import { OrganizationalMapComponent } from './pages/organizational-map/organizational-map.component';
+import { WorkMaintenanceComponent } from './pages/work-maintenance/work-maintenance.component';
+import { CreateParamsComponent } from './pages/create-params/create-params.component';
 
 export const checkListRoutes: Routes = [
   {
     path: '',
     component: CheckListLayoutComponent,
     children: [
-      { path: '', component: CheckListDashboardComponent },
-    ]
-  }
+      { path: 'dashboard', component: CheckListDashboardComponent },
+      { path: 'accesos', component: CheckListAccessComponent },
+      { path: 'cambio-clave', component: ChangePasswordsComponent },
+      { path: 'mapa-organizacional', component: OrganizationalMapComponent },
+      { path: 'obra-mantencion', component: WorkMaintenanceComponent },
+      { path: 'parametros', component: CreateParamsComponent },
+    ],
+  },
 ];
--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -3,11 +3,12 @@ import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatIconModule, MatListModule, CommonModule, MatExpansionModule],
+  imports: [MatIconModule, MatListModule, CommonModule, MatExpansionModule, RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
